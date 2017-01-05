975daad0cf6970920d75980b9d21ace023c2d77f
ameno- <ameno.osman13@gmail.com>
2017-01-05 11:39:18 -0800

    parse sentences and send one complete object to ui

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 
 const express = require('express');
 const watson = require('watson-developer-cloud');
-const app = express();
 const helpers = require('./helpers');
+const app = express();
 
 const tone_analyzer = watson.tone_analyzer({
     username: '9f75e773-a7e2-483e-bc8f-215d087e0341',
@@ -15,18 +15,19 @@ const tone_analyzer = watson.tone_analyzer({
 });
 
 app.get('/:text', function (req, res) {
-    tone_analyzer.tone({ text: req.params.text }, (err, tone) => {
-        if (err){
-            console.log(err);
-            res.send(err);
-        } else {
-            let analytics = helpers.getAnalytics(tone.document_tone.tone_categories);
-            console.log(analytics);
-            res.send(JSON.stringify(analytics, null, 2));
-        }
-    });
+    const sample =  "My man Inf left a Tec and a nine at my crib Turned himself in, he had to do a bid A one-to-three, he be home the end of \'93 I\'m ready to get this paper, G, you with me? Motherfucking right, my pocket\'s looking kind of tight and I'm stressed, yo Biggie let me get the vest No need for that, just grab the fucking gat, The first pocket that\'s fat the Tec is to his back. Word is bond, I\'m a smoke him yo don\'t fake no moves (what?). Treat it like boxing: stick and move, stick and move";
+    tone_analyzer.tone({ text: req.params.text },
+        (err, tone) => {
+            if (err){
+                console.log(err);
+                res.send("Something went wrong! Try selecting some other text");
+            } else {
+                let analytics = helpers.getAnalytics(tone);
+                res.json(analytics);
+            }
+        });
 });
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Think-twise is running on port');
+    console.log('Think twice is running on yo mamas port');
 });
--- helpers.js
+++ helpers.js
@@ -1,6 +1,8 @@
 /**
  * Created by ameno on 1/4/17.
  */
+const colors = require('colors');
+
 function getLabels(tone){
     return tone.tone_name;
 }
@@ -9,14 +11,95 @@ function getScores(tone){
     return (tone.score * 100).toFixed(2);
 }
 
-exports.getAnalytics = (toneCategory) => {
-    let analytics = {};
+function getDocumentAnalytics (toneCategory){
+    let documentScores = {};
     toneCategory.map(function(item){
-        analytics[item.category_id] = {
+        documentScores[item.category_id] = {
             'names': item.tones.map(getLabels),
             'scores': item.tones.map(getScores),
         };
     });
 
-    return analytics;
+    return documentScores;
+}
+
+function getSentenceAnalytics (sentences){
+    let sentenceScores = [];
+    if(sentences) {
+        sentences.map(function (item) {
+            sentenceScores.push({
+                'text': item.text,
+                'tone': getDocumentAnalytics(item.tone_categories),
+            });
+            console.log(sentenceScores[0].tone);
+        });
+    }
+    return sentenceScores;
+}
+
+exports.getAnalytics = (tone) => {
+    return {
+        'documentAnalysis': getDocumentAnalytics(tone.document_tone.tone_categories),
+        'sentenceAnalysis': getSentenceAnalytics(tone.sentences_tone),
+    };
 };
+
+// Data model:
+/*
+{
+     "documentAnalysis": {
+         "emotion_tone": {
+             "names": [],
+             "scores": []
+         },
+         "language_tone": {
+             "names": [],
+             "scores": []
+         },
+         "social_tone": {
+             "names": [],
+             "scores": []
+         }
+ },
+     "sentenceAnalysis": [
+        {
+         "text": "... sentence text ...",
+         "tone": {
+            "emotion_tone": {
+                "names": [],
+                "scores": []
+             },
+            "language_tone": {
+                "names": [],
+                "scores": []
+             },
+            "social_tone": {
+                "names": [],
+                "scores": []
+            },
+         }
+        },
+        {
+         "text": "... sentence text ...",
+         "tone": {
+             "emotion_tone": {
+                 "names": [],
+                 "scores": []
+             },
+             "language_tone": {
+                 "names": [],
+                 "scores": []
+             },
+             "social_tone": {
+                 "names": [],
+                 "scores": []
+             },
+         }
+        },
+     ]
+ }
+*
+*
+*
+*
+* */
